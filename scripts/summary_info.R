1e56b675f186383e797f61a9b40ca61f12632ab6
victoria3109 <50841562+victoria3109@users.noreply.github.com>
2020-05-21 20:44:16 -0700

    Update summary_info.R

--- scripts/summary_info.R
+++ scripts/summary_info.R
@@ -6,10 +6,12 @@ library(styler)
 
 data <- read.csv(file = "data/Countries_with_covid19.csv", stringsAsFactors = FALSE)
 get_info <- function(data){
-  country_with_most_cases <- group_by(Country) %>% summarize(test = sum(Confirmed)) %>% filter(test == max(test)) %>% pull(Country)
-  country_with_least_cases <- group_by(Country) %>% summarize(test = sum(Confirmed)) %>% filter(test == min(test)) %>% pull(Country)
-  most_cases <- group_by(Country) %>% summarize(test = sum(Confirmed)) %>% filter(test == max(test)) %>% pull(test)
-  least_cases <- group_by(Country) %>% summarize(test = sum(Confirmed)) %>% filter(test == min(test)) %>% pull(test)
-  most_deaths <- group_by(Country) %>% summarize(test = sum(Deaths)) %>% filter(test == max(test)) %>% pull(test)
-  info <- list( c(,))
+  country_with_most_cases <- data %>% group_by(Country) %>% summarize(test = sum(Confirmed)) %>% filter(test == max(test)) %>% pull(Country)
+  country_with_least_cases <- data %>% group_by(Country) %>% summarize(test = sum(Confirmed)) %>% filter(test == min(test)) %>% pull(Country)
+  most_cases <- data %>% group_by(Country) %>% summarize(test = sum(Confirmed)) %>% filter(test == max(test)) %>% pull(test)
+  least_cases <- data %>% group_by(Country) %>% summarize(test = sum(Confirmed)) %>% filter(test == min(test)) %>% pull(test)
+  most_deaths <- data %>% group_by(Country) %>% summarize(test = sum(Deaths)) %>% filter(test == max(test)) %>% pull(test)
+  info <- list(country_with_most_cases, country_with_least_cases, most_cases, least_cases, most_deaths)
+  info$length <- length(data)
+  return(info)
 }
