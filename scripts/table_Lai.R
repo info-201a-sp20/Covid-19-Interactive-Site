91874beb3d7bbd324b2533f6620cafb337d480bc
Isaac Lai <isaaclai912@gmail.com>
2020-05-21 17:16:55 -0700

    implemented table

--- scripts/table_Lai.R
+++ scripts/table_Lai.R
@@ -6,17 +6,20 @@ library(styler)
 
 data <- read.csv(file = "data/Countries_with_covid19.csv")
 
-data_table <-
-  data %>%
-  group_by(Country) %>%
-  summarize(
-    "Confirmed Cases" = sum(Confirmed),
-    "Recovered Cases" = sum(Recovered),
-    "Total Deaths" = sum(Deaths)
-  )
-data_table <- data_table[order(-data_table[2]), ]
-# Sorted by number of confirmed cases
-data_table <- top_n(data_table, 20)
-# Only using the twenty highest countries
-
-# Only thing missing is the introduction and interpretation
+table_maker <- function(data){
+  data_table <-
+    data %>%
+    group_by(Country) %>%
+    summarize(
+      "Confirmed Cases" = sum(Confirmed),
+      "Recovered Cases" = sum(Recovered),
+      "Total Deaths" = sum(Deaths)
+    )
+  data_table <- data_table[order(-data_table[2]), ]
+  # Sorted by number of confirmed cases
+  data_table <- top_n(data_table, 20)
+  # Only using the twenty highest countries
+  
+  # Only thing missing is the introduction and interpretation
+  return(data_table)
+}
