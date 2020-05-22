ef8d0a4e473cda5a28ab6104fd6d2c7e1349960a
Isaac Lai <isaaclai912@gmail.com>
2020-05-21 22:38:03 -0700

    finished summinfo

--- scripts/summary_info.R
+++ scripts/summary_info.R
@@ -6,29 +6,25 @@ library(styler)
 
 data <- read.csv(file = "data/Countries_with_covid19.csv", stringsAsFactors = FALSE)
 get_info <- function(data) {
+  grouped_data <- data %>% group_by(Country)
   country_with_most_cases <-
-    group_by(Country) %>%
-    summarize(test = sum(Confirmed)) %>%
+    summarize(grouped_data, test = sum(Confirmed)) %>%
     filter(test == max(test)) %>%
     pull(Country)
   country_with_least_cases <-
-    group_by(Country) %>%
-    summarize(test = sum(Confirmed)) %>%
+    summarize(grouped_data, test = sum(Confirmed)) %>%
     filter(test == min(test)) %>%
     pull(Country)
   most_cases <-
-    group_by(Country) %>%
-    summarize(test = sum(Confirmed)) %>%
+    summarize(grouped_data, test = sum(Confirmed)) %>%
     filter(test == max(test)) %>%
     pull(test)
   least_cases <-
-    group_by(Country) %>%
-    summarize(test = sum(Confirmed)) %>%
+    summarize(grouped_data, test = sum(Confirmed)) %>%
     filter(test == min(test)) %>%
     pull(test)
   most_deaths <-
-    group_by(Country) %>%
-    summarize(test = sum(Deaths)) %>%
+    summarize(grouped_data, test = sum(Deaths)) %>%
     filter(test == max(test)) %>%
     pull(test)
   info <- list(
@@ -39,5 +35,24 @@ get_info <- function(data) {
     most_deaths
   )
 }
-
-get_info(data)
+grouped_data <- data %>% group_by(Country)
+country_with_most_cases <-
+  summarize(grouped_data, test = sum(Confirmed)) %>%
+  filter(test == max(test)) %>%
+  pull(Country)
+country_with_least_cases <-
+  summarize(grouped_data, test = sum(Confirmed)) %>%
+  filter(test == min(test)) %>%
+  pull(Country)
+most_cases <-
+  summarize(grouped_data, test = sum(Confirmed)) %>%
+  filter(test == max(test)) %>%
+  pull(test)
+least_cases <-
+  summarize(grouped_data, test = sum(Confirmed)) %>%
+  filter(test == min(test)) %>%
+  pull(test)
+most_deaths <-
+  summarize(grouped_data, test = sum(Deaths)) %>%
+  filter(test == max(test)) %>%
+  pull(test)
