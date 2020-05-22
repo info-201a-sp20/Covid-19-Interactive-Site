0a463ab568a11b261f75b6c710fa86deaa34e5cc
Isaac Lai <isaaclai912@gmail.com>
2020-05-21 18:34:58 -0700

    started the summinfo

--- scripts/summary_info.R
+++ scripts/summary_info.R
@@ -6,6 +6,10 @@ library(styler)
 
 data <- read.csv(file = "data/Countries_with_covid19.csv", stringsAsFactors = FALSE)
 get_info <- function(data){
-  country_with_most <- group_by(Country) %>% summarize(test = sum())
-  country_with_least
+  country_with_most_cases <- group_by(Country) %>% summarize(test = sum(Confirmed)) %>% filter(test == max(test)) %>% pull(Country)
+  country_with_least_cases <- group_by(Country) %>% summarize(test = sum(Confirmed)) %>% filter(test == min(test)) %>% pull(Country)
+  most_cases <- group_by(Country) %>% summarize(test = sum(Confirmed)) %>% filter(test == max(test)) %>% pull(test)
+  least_cases <- group_by(Country) %>% summarize(test = sum(Confirmed)) %>% filter(test == min(test)) %>% pull(test)
+  most_deaths <- group_by(Country) %>% summarize(test = sum(Deaths)) %>% filter(test == max(test)) %>% pull(test)
+  info <- list( c(,))
 }
